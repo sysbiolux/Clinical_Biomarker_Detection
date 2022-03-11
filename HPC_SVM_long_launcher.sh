2596e5a1614bcc32d1f9ff7b766bb1a3401c89a5
Jeff Didier <38098941+Moshkante@users.noreply.github.com>
2022-03-11 13:56:38 +0100

    Update and rename source/HPC_SVM_long_launcher.sh to HPC_SVM_long_launcher.sh

--- HPC_SVM_long_launcher.sh
+++ HPC_SVM_long_launcher.sh
@@ -8,7 +8,7 @@
 #SBATCH --qos=long
 #SBATCH -p batch
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-use=jeff.didier@uni.lu
+#SBATCH --mail-use=username@uni.lu
 
 # Safeguard for NOT running this launcher on access/login nodes
 print_error_and_exit() { echo "***ERROR*** $*"; exit 1; }
