128dbfc40f699f235b96ec03e522266e9ee18f90
Jeff Didier <38098941+Moshkante@users.noreply.github.com>
2023-01-25 10:38:14 +0100

    Update CBDP_utils.py

--- source/CBDP_utils.py
+++ source/CBDP_utils.py
@@ -1,7 +1,7 @@
 ########################################################################################################################
 # SCRIPT WITH ALL UTIL FUNCTIONS FOR CLINICAL DATA ANALYSIS ############################################################
 # Jeff DIDIER - Faculty of Science, Technology and Medicine (FSTM), Department of Life Sciences and Medicine (DLSM) ####
-# November 2021 - October 2022, University of Luxembourg, v.11/22/2022 (M/d/y) #########################################
+# November 2021 - January 2023, University of Luxembourg, v.01/25/2023 (M/d/y) #########################################
 ########################################################################################################################
 
 # Script of util functions for the classification and evaluation of predictive machine learning models to detect
